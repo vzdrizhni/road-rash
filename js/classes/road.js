af6fcb53446dfa14677a141817769a5fe5719432
vzdrizhni <vzdrizhni@gmail.com>
2020-07-18 19:34:28 +0500

    Lines on the road

--- js/classes/road.js
+++ js/classes/road.js
@@ -5,5 +5,21 @@ class Road extends Phaser.GameObjects.Container {
     this.back = this.scene.add.image(0,0,'road');
     this.add(this.back);
     this.scene.add.existing(this);
+
+    // this.back.displayWidth = game.config.width * 0.5;
+    // this.back.scaleY = this.back.scaleX;
+    Align.scaleToGameW(this.back, 0.5);
+
+    this.setSize(this.back.displayWidth, game.config.height);
+
+    this.lineGroup = this.scene.add.group();
   }
+  makeLines() {
+    this.vSpace = this.displayHeight / 10;
+
+    for (let i = 0; i < 100; i++) {
+      let line = this.scene.add.image(this.x, this.vSpace * i, 'line');
+      this.lineGroup.add(line);
+    }
+  };
 }
--- js/sceneMain.js
+++ js/sceneMain.js
@@ -5,11 +5,13 @@ class SceneMain extends Phaser.Scene {
   preload()
   {
     this.load.image('road', './images/road.jpg');
+    this.load.image('line', './images/line.png');
     this.load.spritesheet('cars', './images/cars.png', {frameWidth: 60, frameHeight: 126});
   }
   create() {
     let road = new Road({scene: this});
     road.x = game.config.width / 2;
+    road.makeLines();
   }
   update() {}
 }
